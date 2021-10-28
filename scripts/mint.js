1fac5a6b048b7d14cfd4cf6ea05aefc1fe314ea9
benm4nn <benm4nn@gmail.com>
2021-10-28 09:09:10 +0300

    udpates

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,21 +1,263 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const web3 = require("web3");
+require('dotenv').config();
+const fs = require('fs');
+const path = require("path");
+
+const INFURA_KEY = process.env.INFURA_KEY;
+const MATIC_KEY = process.env.MATIC_KEY;
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
 const MNEMONIC = process.env.MNEMONIC;
-const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
+const BASE_URL = process.env.BASE_URL;
+
+var NODE_API_KEY;
 const isInfura = !!process.env.INFURA_KEY;
-const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
+
+//const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
 const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
-const NETWORK = process.env.NETWORK;
-const NUM_CREATURES = 12;
+//const NETWORK = process.env.NETWORK;
+
+//NUM_CREATURES = 12;
 const NUM_LOOTBOXES = 4;
 const DEFAULT_OPTION_ID = 0;
 const LOOTBOX_OPTION_ID = 2;
 
+//Process the arguments
+const args = process.argv.slice(2);
+const argMap = {};
+
+if (args.length % 2)
+  throw new Error('Invalid number of arguments');
+
+for (let i = 0; i < args.length; i += 2) {
+  let key = args[i];
+
+  if (!/^--([a-z]+-)*[a-z]+$/g.test(key))
+    throw new Error('Invalid argument name');
+
+  key = key
+    .replace(/^--/, '')
+    .replace(/-([a-z])/g, g => g[1].toUpperCase());
+
+  argMap[key] = args[i + 1];
+};
+
+//POST PROCESS the argMap
+var CHAIN_URL;
+var Options;
+
+if (argMap.network) {
+  switch (argMap.network.toLowerCase()) {
+    case ("mainnet" || "live"):
+      NODE_API_KEY = isInfura ? INFURA_KEY : ALCHEMY_KEY;
+      CHAIN_URL = isInfura ? "https://" + network + ".infura.io/v3/" + INFURA_KEY : "https://eth-" + network + ".alchemyapi.io/v2/" + ALCHEMY_KEY;
+      Options = {
+        gas: 5000000,
+        gasPrice: 5000000000,
+        chainId: 1
+      };
+      break;
+    case "rinkeby":
+      NODE_API_KEY = ALCHEMY_KEY;
+      CHAIN_URL = isInfura ? "" : "";
+      Options = {
+        gas: 8000000,
+        network_id: 4,
+        chainId: 4
+      };
+      break;
+    case "mumbai":
+      NODE_API_KEY = MATIC_KEY;
+      CHAIN_URL = `https://rpc-mumbai.maticvigil.com/v1/${MATIC_KEY}`;
+      Options = {
+        network_id: 80001,
+        confirmations: 2,
+        skipDryRun: true,
+        //If you don't set a gas fee, it will assume you have no gas and give the error 
+        // Error:  *** Deployment Failed ***
+        //"Migrations" could not deploy due to insufficient funds
+        gas: 3000000,
+        gasPrice: 10000000000,
+        chainId: 80001
+      };
+      break;
+    case "matic":
+      NODE_API_KEY = MATIC_KEY;
+      CHAIN_URL = `https://rpc-mainnet.maticvigil.com/v1/${MATIC_KEY}`;
+      Options = {
+        network_id: 137,
+        // gas: 5000000,
+        // gasPrice: 5000000000,
+        confirmations: 2,
+        chainId: 137
+      };
+      break;
+    case "":
+      console.error("Please set a valid network using argument --network <live, mainnet, rinkeby, mumbai, matic>");
+      return;
+      break;
+    default:
+      console.error("Please set a valid network, " + argMap.network.toLowerCase() + " is not valid. Use --network <live, mainnet, rinkeby, mumbai, matic>");
+      return;
+      break;
+  }
+};
+
+//argument --n 8 means mint 8 items
+var NUM_CREATURES = 1;
+if (argMap.n) {
+  NUM_CREATURES = parseInt(argMap.n, 10);
+};
+
+//* Remember to write the nft address in the .env file after deploying the contract
+//const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS
+//const OWNER_ADDRESS = process.env.OWNER_ADDRESS
+
+const MUMBAI = `https://rpc-mumbai.maticvigil.com/v1/${NODE_API_KEY}`
+//const MATIC = `https://rpc-mainnet.maticvigil.com/v1/${NODE_API_KEY}`
+//const MAX_NUMBER = 10000;
+//const NUM_ITEMS = 3;
+
+//*Parse the contract artifact for ABI reference.
+let rawdata = fs.readFileSync(path.resolve(__dirname, "../build/contracts/Creature.json"));
+
+let contractAbi = JSON.parse(rawdata);
+const NFT_ABI = contractAbi.abi
+
+async function main() {
+
+  try {
+    //*define web3, contract and wallet instances
+    const provider = new HDWalletProvider(
+      MNEMONIC,
+      `https://rpc-mumbai.maticvigil.com/v1/${NODE_API_KEY}`/*,
+      {
+        network_id: 80001,
+        confirmations: 2,
+        skipDryRun: true,
+        //If you don't set a gas fee, it will assume you have no gas and give the error 
+        // Error:  *** Deployment Failed ***
+        //"Migrations" could not deploy due to insufficient funds
+        gas: 3000000,
+        gasPrice: 10000000000
+      }
+      */
+    );
+
+    const web3Instance = new web3(provider);
+
+    const nftContract = new web3Instance.eth.Contract(
+      NFT_ABI,
+      NFT_CONTRACT_ADDRESS,
+      Options
+    );
+
+    //* just mint 
+    /*
+    await nftContract.methods
+    .mintItem(OWNER_ADDRESS, "https://krazyphaces.herokuapp.com/api/token/")
+    .send({ from: OWNER_ADDRESS }).then(console.log('minted')).catch(error => console.log(error));
+    */
+
+    for (var i = 0; i < NUM_CREATURES; i++) {
+      const result = await nftContract.methods
+      //mint function needs to be found in the abi i.e. search in /build/contracts/Creature.json for the minting function
+      //https://ethereum.stackexchange.com/questions/65370/contract-methods-transfer-is-a-not-a-function-error-using-web3
+       // .mintTo(OWNERS_ADDRESS, BASE_URL)
+       .mintTo(OWNER_ADDRESS)
+        .send({ from: OWNER_ADDRESS, gasPrice: "150000000000", gas: "3000000", chainId: 80001, });
+      console.log("Minted NFT " + (i+1) + ". Transaction: " + result.transactionHash);
+    }
+
+    /*
+    for (var i = 1; i <= NUM_CREATURES; i++) {
+      await nftContract.methods
+        .mint(OWNER_ADDRESS, BASE_URL)
+        .send({ from: OWNER_ADDRESS }).then(console.log('minted nft ' + i)).catch(error => console.log(error));
+    }
+    */
+
+  }
+
+  catch (e) {
+    console.log(e)
+  }
+}
+
+//invoke
+main().then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+development: {
+  host: "localhost",
+  port: 7545,
+  gas: 5000000,
+  network_id: "*", // Match any network id
+},
+mumbai: {
+  provider: function () {
+    return new HDWalletProvider(MNEMONIC, `https://rpc-mumbai.maticvigil.com/v1/${MATIC_KEY}`);
+  },
+  network_id: 80001,
+  confirmations: 2,
+  skipDryRun: true,
+  //If you don't set a gas fee, it will assume you have no gas and give the error
+  // Error:  *** Deployment Failed ***
+  //"Migrations" could not deploy due to insufficient funds
+  gas: 3000000,
+  gasPrice: 10000000000
+},
+matic: {
+  provider: function () {
+    return new HDWalletProvider(MNEMONIC, `https://rpc-mainnet.maticvigil.com/v1/${MATIC_KEY}`);
+  },
+  network_id: 137,
+ // gas: 5000000,
+//  gasPrice: 5000000000,
+  confirmations: 2,
+},
+rinkeby: {
+  provider: function () {
+    return new HDWalletProvider(MNEMONIC, rinkebyNodeUrl);
+  },
+  //gas: 5000000,
+  gas: 8000000,
+  network_id: 4,
+},
+live: {
+  network_id: 1,
+  provider: function () {
+    return new HDWalletProvider(MNEMONIC, mainnetNodeUrl);
+  },
+  gas: 5000000,
+  gasPrice: 5000000000,
+},
+*/
+
+/*
 if (!MNEMONIC || !NODE_API_KEY || !OWNER_ADDRESS || !NETWORK) {
-  console.error(
-    "Please set a mnemonic, Alchemy/Infura key, owner, network, and contract address."
-  );
+  console.error("Please set a mnemonic, RPC API key, owner, network, and contract address.");
   return;
 }
 
@@ -57,14 +299,12 @@ const FACTORY_ABI = [
   },
 ];
 
+
 async function main() {
-  const network =
-    NETWORK === "mainnet" || NETWORK === "live" ? "mainnet" : "rinkeby";
+
   const provider = new HDWalletProvider(
     MNEMONIC,
-    isInfura
-      ? "https://" + network + ".infura.io/v3/" + NODE_API_KEY
-      : "https://eth-" + network + ".alchemyapi.io/v2/" + NODE_API_KEY
+    CHAIN_URL
   );
   const web3Instance = new web3(provider);
 
@@ -72,7 +312,7 @@ async function main() {
     const factoryContract = new web3Instance.eth.Contract(
       FACTORY_ABI,
       FACTORY_CONTRACT_ADDRESS,
-      { gasLimit: "1000000" }
+      Options
     );
 
     // Creatures issued directly to the owner.
@@ -112,3 +352,6 @@ async function main() {
 }
 
 main();
+
+
+*/
--- truffle.js
+++ truffle.js
@@ -1,3 +1,23 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
 require('dotenv').config();
@@ -30,6 +50,16 @@ const mainnetNodeUrl = isInfura
   ? "https://mainnet.infura.io/v3/" + NODE_API_KEY
   : "https://eth-mainnet.alchemyapi.io/v2/" + NODE_API_KEY;
 
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
 module.exports = {
   networks: 
   {
@@ -45,22 +75,25 @@ module.exports = {
       },
       network_id: 80001,
       confirmations: 2,
-      skipDryRun: true
+      skipDryRun: true,
+      gas: 3000000,
+      gasPrice: 10000000000
     },
     matic: {
       provider: function () {
         return new HDWalletProvider(MNEMONIC, `https://rpc-mainnet.maticvigil.com/v1/${MATIC_KEY}`);
       },
       network_id: 137,
-      gas: 5000000,
-      gasPrice: 5000000000,
+     // gas: 5000000,
+    //  gasPrice: 5000000000,
       confirmations: 2,
     },
     rinkeby: {
       provider: function () {
         return new HDWalletProvider(MNEMONIC, rinkebyNodeUrl);
       },
-      gas: 5000000,
+      //gas: 5000000,
+      gas: 8000000,
       network_id: 4,
     },
     live: {
